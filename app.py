f1d45bfc18071e2aaa907b5c3103610cf9b3628f
Sammiemigwi <89806039+Sammiemigwi@users.noreply.github.com>
2023-11-08 10:04:24 +0000

    Added Jobs

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title' : 'Software Engineering',
+    'location' : 'Nairobi, Kenya',
+    'salary' : '120,000ksh'
+  },
+
+  {
+    'id': 2,
+    'title' : 'Accountant',
+    'location' : 'Nairobi, Kenya',
+    'salary' : '80,000ksh'
+  },
+
+  {
+    'id': 3,
+    'title' : 'Project Mananger',
+    'location' : 'Nakuru, Kenya',
+    'salary' : '70,000ksh'
+  },
+
+  {
+    'id': 4,
+    'title' : 'Web Designer',
+    'location' : 'Mombasa, Kenya',
+  
+  },
+
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",jobs = JOBS)
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__== "__main__":
   app.run(host='0.0.0.0' , debug=True)
   
